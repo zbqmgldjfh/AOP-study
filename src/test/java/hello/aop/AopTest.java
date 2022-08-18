c1c1f0a9de91701140faff3ecc186fde670a35fc
jiwoo <zbqmgldjfh@gmail.com>
2022-08-18 15:21:28 +0900

    feat(AspectV6): Advice의 종류에 대하여 학습하기
    - Around, Before, AfterReturning, AfterThrowing, After

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -2,8 +2,7 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import hello.aop.order.aop.AspectV4Pointcut;
-import hello.aop.order.aop.AspectV5Order;
+import hello.aop.order.aop.AspectV6Advice;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -19,7 +18,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 //@Import(AspectV2.class)
 //@Import(AspectV3.class)
 //@Import(AspectV4Pointcut.class)
-@Import({AspectV5Order.logAspect.class, AspectV5Order.TxAspect.class})
+//@Import({AspectV5Order.logAspect.class, AspectV5Order.TxAspect.class})
+@Import(AspectV6Advice.class)
 public class AopTest {
 
     @Autowired
